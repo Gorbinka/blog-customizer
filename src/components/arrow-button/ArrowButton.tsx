3491c69e9a43e2c0861f5ab20c8eb319324b003e
Gorbinka <daniilgorbacev20109@yandex.ru>
2024-07-19 15:00:48 +0300

    1

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -2,18 +2,32 @@ import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
-/** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+import { classNames } from 'src/utils/utils';
 
-export const ArrowButton = () => {
+/** Функция для обработки открытия/закрытия формы */
+export type OnClick = (
+	event: React.MouseEvent<HTMLDivElement, MouseEvent>
+) => void;
+
+interface ArrowButtonProps {
+	onClick?: OnClick;
+	isOpen?: boolean;
+}
+
+export const ArrowButton = ({ onClick, isOpen }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={classNames(styles.container, isOpen && styles.container_open)}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={classNames(styles.arrow, isOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,19 +1,30 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
-
+// import { ArrowButton } from 'components/arrow-button';
+// import { Button } from 'components/button';
+import { ReactNode } from 'react';
+import { classNames } from 'src/utils/utils';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+interface PropsArticleParamsForm {
+	children?: ReactNode;
+	arrowButton?: ReactNode;
+	isOpen?: boolean;
+}
+
+export const ArticleParamsForm = ({
+	children,
+	arrowButton,
+	isOpen,
+}: PropsArticleParamsForm) => {
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
+			{arrowButton}
+			<aside
+				className={classNames(
+					styles.container,
+					isOpen && styles.container_open
+				)}
+				onClick={(event) => event.stopPropagation()}>
+				<form className={styles.form}>{children}</form>
 			</aside>
 		</>
 	);
